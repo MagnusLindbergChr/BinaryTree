42a95f56077629d8a342981e26db6fb217945d92
Magnus Lindberg <magnus.lindberg.chr@gmail.com>
2020-03-15 14:39:17 +0100

    Created necessary classes

---
+++ BinaryTree/BinaryTree/TreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    public class TreeNode<T>
+    {
+        // Contains the value of the treenode
+        private T value;
+        // Shows whether the current node has a parent or not
+        private bool hasParent;
+        // Contains the children of the node (zero or more)
+        private List<TreeNode<T>> children;
+
+        // Construct a tree node, "Value" is the value of the node
+        public TreeNode(T value)
+        {
+            if (value != null)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
