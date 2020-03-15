24f06d923a1d6a96eee6b0ab0ff95c29f7a1e1b9
Magnus Lindberg <magnus.lindberg.chr@gmail.com>
2020-03-15 15:41:52 +0100

    Created root insert behaviour

--- BinaryTree/BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace BinaryTree
 {
     public class BinaryTree
     {
-        public BinaryTree()
+        // Store data for the tree root
+        private TreeNode root;
+        public TreeNode Root
         {
+            get { return root; }
+        }
+
+        // Insert behaviour for the root
+        public void Insert(int data)
+        {
+            // if root not null... call insert on the root node
+            if(root != null)
+            {
+                root.Insert(data);
+            }
+            // if the root is null... Create root
+            else
+            {
+                root = new TreeNode(data);
+            }
         }
     }
 }
