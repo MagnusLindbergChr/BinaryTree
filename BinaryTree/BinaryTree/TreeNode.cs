823b218ba34d2da34b3aacd67a47a6c93749b516
Magnus Lindberg <magnus.lindberg.chr@gmail.com>
2020-03-15 15:37:52 +0100

    Made treeNode behaviour

--- BinaryTree/BinaryTree/TreeNode.cs
+++ BinaryTree/BinaryTree/TreeNode.cs
@@ -3,22 +3,14 @@ using System.Collections.Generic;
 
 namespace BinaryTree
 {
-    public class TreeNode<T>
+    public class TreeNode
     {
-        // Contains the value of the treenode
-        private T value;
-        // Shows whether the current node has a parent or not
-        private bool hasParent;
-        // Contains the children of the node (zero or more)
-        private List<TreeNode<T>> children;
 
-        // Construct a tree node, "Value" is the value of the node
-        public TreeNode(T value)
+        private int data;
+
+        public TreeNode(int value)
         {
-            if (value != null)
-            {
-                this.value = value;
-            }
+            data = value;
         }
     }
 }
